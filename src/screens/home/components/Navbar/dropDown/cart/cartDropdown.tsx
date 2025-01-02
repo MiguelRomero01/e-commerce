9df36aeb7121573dc50f0bd3e1a70832e140719c
miguel angel romero parra <mr9501459@gmail.com>
2025-01-02 18:26:23 -0500

    commit

--- src/screens/home/components/Navbar/dropDown/cart/cartDropdown.tsx
+++ src/screens/home/components/Navbar/dropDown/cart/cartDropdown.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import cartDropdown_styles from './cartDropdown.module.css';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Quantity from "./components/quantity";
+import { Delete } from "@mui/icons-material";
 
 const CartDropdown = () => {
      const [showDropdownCart, setShowDropdownCart] = useState(false);
@@ -21,10 +23,48 @@ const CartDropdown = () => {
 
                {showDropdownCart && (
                     <div className={cartDropdown_styles['dropdown-menu']}>
-                         <ul>
-                              <li>Cart</li>
-                              <li>Checkout</li>
-                         </ul>
+                         {Products.length === 0 ? (
+                              <p className={cartDropdown_styles.emptyCart}>No products in cart</p>
+                         ) : (
+                              <>
+                                   <ul className={cartDropdown_styles.cartList}>
+                                        {Products.map((productCart) => (
+                                             
+                                             <li key={productCart.id} className={cartDropdown_styles.cartItem}>
+                                                  <div className={cartDropdown_styles.productImage}>
+                                                       <img src={productCart.ImageURL} alt={productCart.Title} />
+                                                  </div>
+                                                  <div className={cartDropdown_styles.productInfo}>
+                                                       <h3>{productCart.Title}</h3>
+                                                       <p className={cartDropdown_styles.price}>${productCart.Price * quantity}</p>
+                                                       <Quantity quantity={quantity} setQuantity={setQuantity}/>
+                                                  </div>
+
+                                                  <button className={cartDropdown_styles['remove-product']}>
+                                                       <Delete 
+                                                            sx={{ 
+                                                                 fontSize: '1.1rem', 
+                                                                 verticalAlign: 'middle',
+                                                                 color: 'white',
+                                                                 "&:hover":{
+                                                                      color: 'black',
+                                                                 }
+                                                                 }}
+                                                            />
+                                                  </button>
+                                             </li>
+                                             
+                                        ))
+                                   
+                                   }
+                                   </ul>
+                                   <div className={cartDropdown_styles.cartFooter}>
+                                        <button className={cartDropdown_styles.checkoutButton}>
+                                             Checkout
+                                        </button>
+                                   </div>
+                              </>
+                         )}
                     </div>
                )} 
           </div>
