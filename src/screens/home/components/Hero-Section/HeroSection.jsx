207b22f5904d39aec9f78f69e907888dc81917dc
miguel angel romero parra <mr9501459@gmail.com>
2025-01-01 21:07:22 -0500

    fixing bugs
    for some reason the image i used before that the current, it was showed by the navigator, so i changed the image's name

--- src/screens/home/components/Hero-Section/HeroSection.jsx
+++ src/screens/home/components/Hero-Section/HeroSection.jsx
@@ -34,16 +34,16 @@ export default function HeroSection () {
           <div className={HeroStyles.heroSection}>
             <div className={HeroStyles['heroSectionContent-left']}>
               <h1 className={HeroStyles['main-title']}>
-                <span>LET'S</span> <br />
-                <span>EXPLORE</span> <br />
-                <span>UNIQUE</span> <br />
-                <span>CLOTHES</span>
+                <span>{displayText.line1}</span> <br />
+                <span>{displayText.line2}</span> <br />
+                <span>{displayText.line3}</span> <br />
+                <span>{displayText.line4}</span>
               </h1>
               <p className={HeroStyles.text}>Live for influential and innovative fashion!</p>
               <button className={HeroStyles['main-button']}>Get Started</button>
             </div>
 
-            <div style={{marginBottom:'40em'}}>
+            <div style={{marginBottom:'70vh'}}>
               <SocialMedia/>
             </div>
 
