66fdbd042d02fdc4af50ee5ef2504fc314e80f84
miguel angel romero parra <mr9501459@gmail.com>
2025-01-16 18:46:52 -0500

    memberships

--- src/screens/memberships/memberships.tsx
+++ src/screens/memberships/memberships.tsx
@@ -4,6 +4,8 @@ import Membership from "./components/membership";
 
 import { CartDropdown_ProductsType } from "../../features/services/Cart/CartDropdownProducts";
 import { Navbar } from "../../common/components/Navbar";
+import { Crown, Rocket, Zap } from "lucide-react";
+import { Footer } from "../../common/components/Footer";
 
 interface membershipsProps {
   isLogged: boolean;
@@ -30,28 +32,45 @@ const membershipScreen: React.FC<membershipsProps> = ({
         />
       </header>
 
-      <section id={membershipStyles["memberships-section-container"]}>
-        <Membership
-          title="Amateur"
-          includesText="Include"
-          price={3200}
-          listOfIncludes={["asdas", "dasdas", "sfas"]}
-        />
+      <div className={membershipStyles["memberships-container"]}>
+        <section className={membershipStyles["memberships-container-header"]}>
+          <h2>Choose the perfect plan for you</h2>
+          <p>
+            We offer different levels of licenses to suit your needs. From
+            entrepreneurs to large companies, we have the ideal solution for you.
+          </p>
+        </section>
 
-        <Membership
-          title="Pro"
-          includesText="Include Amateur, plus"
-          price={3200}
-          listOfIncludes={["asdas", "dasdas", "sfas"]}
-        />
-        
-        <Membership
-          title="Business"
-          includesText="Include Pro, plus"
-          price={3200}
-          listOfIncludes={["asdas", "dasdas", "sfas"]}
-        />
-      </section>
+        <section id={membershipStyles["memberships-section-container"]}>
+          <Membership
+            title="Amateur"
+            includesText="Include"
+            price={10000}
+            listOfIncludes={["asdas", "dasdas", "sfas"]}
+            icon={<Zap size={30} />}
+          />
+
+          <Membership
+            title="Pro"
+            includesText="Include Amateur, plus"
+            price={20000}
+            listOfIncludes={["asdas", "dasdas", "sfas", "sdasd", "fasd"]}
+            icon={<Rocket size={30} />}
+          />
+
+          <Membership
+            title="Business"
+            includesText="Include Pro, plus"
+            price={30000}
+            listOfIncludes={["asdas", "dasdas", "sfas", "gas"]}
+            icon={<Crown size={30} />}
+          />
+        </section>
+      </div>
+
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 };
